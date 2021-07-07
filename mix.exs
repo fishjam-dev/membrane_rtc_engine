4bba221bd42b0387146ec053e901519093039775
Michał Śledź <michal.sledz@swmansion.com>
2021-07-07 17:50:02 +0200

    Add mix aliases for compiling TS source code with Elixir one and generating TS docs with Elixir ones

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Membrane.SFU.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -28,5 +29,31 @@ defmodule Membrane.SFU.MixProject do
       {:dialyxir, "1.1.0", only: :dev, runtime: false},
       {:ex_doc, "0.24.2", only: :dev, runtime: false}
     ]
+  end
+
+  defp aliases() do
+    [
+      compile: ["compile", &compile_ts/1],
+      docs: ["docs", &generate_ts_docs/1]
+    ]
+  end
+
+  defp compile_ts(_) do
+    Mix.shell().info("Installing npm dependencies")
+    {result, exit_status} = System.cmd("npm", ["install"], cd: "assets")
+    Mix.shell().info(result)
+    if exit_status != 0, do: raise("Failed to install npm dependecies")
+
+    Mix.shell().info("Compiling TS files")
+    {result, exit_status} = System.cmd("npm", ["run", "build"], cd: "assets")
+    Mix.shell().info(result)
+    if exit_status != 0, do: raise("Failed to compile TS files")
+  end
+
+  defp generate_ts_docs(_) do
+    Mix.shell().info("Generating TS docs")
+    {result, exit_status} = System.cmd("npm", ["run", "docs"], cd: "assets")
+    Mix.shell().info(result)
+    if exit_status != 0, do: raise("Failed to generate TS docs")
   end
 end
--- priv/static/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * some test docs
- * @module
- */
-export { Peer, PeerConfig, MembraneWebRTCConfig, MembraneWebRTC, TrackContext, Callbacks, } from "./membraneWebRTC";
-export { SerializedMediaEvent } from "./mediaEvent";
-//# sourceMappingURL=index.d.ts.map
