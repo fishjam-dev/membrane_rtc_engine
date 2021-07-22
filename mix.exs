a216e735d24b625555ae4041f6b0b8b57d11ce73
Michał Śledź <michal.sledz@swmansion.com>
2021-07-22 18:02:52 +0200

    Update membrane_webrtc_plugin

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTC.Engine.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.0-alpha"
   @github_url "https://github.com/membraneframework/membrane_rtc_engine"
 
   def project do
@@ -39,7 +39,7 @@ defmodule Membrane.RTC.Engine.MixProject do
   defp deps do
     [
       {:membrane_core, "~> 0.7.0", override: true},
-      {:membrane_webrtc_plugin, github: "membraneframework/membrane_webrtc_plugin"},
+      {:membrane_webrtc_plugin, "0.1.0-alpha"},
       {:membrane_element_tee, "~> 0.4.1"},
       {:membrane_element_fake, "~> 0.4.0"},
       {:jason, "~> 1.2"},
