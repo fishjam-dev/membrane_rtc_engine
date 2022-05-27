6b15767c8d439ef9f533790d413611455b88db65
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2022-05-27 17:42:10 +0200

    Bump version to 0.3.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTC.Engine.MixProject do
   use Mix.Project
 
-  @version "0.3.1"
+  @version "0.3.2"
   @github_url "https://github.com/membraneframework/membrane_rtc_engine"
 
   def project do
