4faf3581ab2ae07b75d6622922aca6e6e20065b8
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-07-07 15:07:17 +0200

    Bump version to 0.5.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTC.Engine.MixProject do
   use Mix.Project
 
-  @version "0.5.0"
+  @version "0.5.1"
   @github_url "https://github.com/membraneframework/membrane_rtc_engine"
 
   def project do
