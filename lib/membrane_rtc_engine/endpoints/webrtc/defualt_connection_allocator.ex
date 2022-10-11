f17b5faafd7cec9e2b5d498372c98b2d64307b82
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-10-11 16:18:09 +0200

    CR suggestions

---
+++ lib/membrane_rtc_engine/endpoints/webrtc/defualt_connection_allocator.ex
@@ -0,0 +1,20 @@
+defmodule Membrane.RTC.Engine.Endpoint.WebRTC.DefaultConnectionAllocator do
+  @moduledoc """
+  Implementation of `Membrane.RTC.Engine.Endpoint.WebRTC.ConnectionAllocator` that does nothing.
+
+  It might be useful for non-WebRTC Endpoints
+  """
+  @behaviour Membrane.RTC.Engine.Endpoint.WebRTC.ConnectionAllocator
+
+  @impl true
+  def register_track_receiver(_manager, _pid), do: :ok
+
+  @impl true
+  def update_bandwidth_estimation(_manager, _estimation), do: :ok
+
+  @impl true
+  def probe_sent(_manager), do: :ok
+
+  @impl true
+  def buffer_sent(_manager, _buffer), do: :ok
+end
