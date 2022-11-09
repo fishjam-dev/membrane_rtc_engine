d4a68427cda2bcb02fd797898a38af36fe277fc2
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-11-09 10:23:15 +0100

    Remove DefaultConnectionAllocator
    This file shouldn't be present. It was removed some time ago and is probably a rebase error

--- config/test.ex
+++ config/test.ex
@@ -1,4 +1,4 @@
 import Config
 
 config :membrane_rtc_engine,
-  connection_prober_implementation: Membrane.RTC.Engine.Endpoint.WebRTC.DefaultConnectionAllocator
+  connection_prober_implementation: Membrane.RTC.Engine.Endpoint.WebRTC.NoOpConnectionAllocator
