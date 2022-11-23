690dd485e370977abae85f61f1cb22d50e40ba3d
KarolK99 <konkolisz@gmail.com>
2022-11-23 11:29:01 +0100

    Change requests

--- lib/membrane_rtc_engine/endpoints/hls/ffmpeg_filter.ex
+++ lib/membrane_rtc_engine/endpoints/hls/ffmpeg_filter.ex
@@ -1,6 +1,17 @@
 defmodule Membrane.RTC.Engine.Endpoint.HLS.FFmpegFilter do
   @moduledoc """
-  Determines how compositor output will look be delivering ffmpeg filter graph.
+  Returns FFmpeg filtergraph that is used to create compositor output.
+  https://ffmpeg.org/ffmpeg-filters.html#toc-Filtergraph-description
+
+  Used ffmpeg options:
+    force_original_aspect_ratio=decrease - the output video dimensions will automatically be decreased if needed.
+    setsar - sets the Sample (aka Pixel) Aspect Ratio for the filter output video.
+    iw/ih - width/height of the input video.
+    ow/oh - the output (cropped) width and height.
+    pad - add paddings to the input image, and place the original input at the provided x, y coordinates.
+    crop - the input video to given dimensions.
+    vstack - stack input videos vertically.
+    hstack - stack input videos horizontally.
   """
 
   @spec default_ffmpeg_filter(integer(), integer(), integer()) :: binary()
@@ -12,35 +23,32 @@ defmodule Membrane.RTC.Engine.Endpoint.HLS.FFmpegFilter do
 
   def default_ffmpeg_filter(width, height, 2) do
     """
-    [0:v]scale=-1:#{height}:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[left];
-    [1:v]scale=-1:#{height}:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[right];
-    [left][right]hstack
+    [0:v]scale=-1:#{height}:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[left];\
+    [1:v]scale=-1:#{height}:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[right];\
+    [left][right]hstack\
     """
-    |> String.replace("\n", "", global: true)
   end
 
   def default_ffmpeg_filter(width, height, 3) do
     """
-    [0:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[lefttop];
-    [1:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[leftdown];
-    [2:v]scale=-1:#{height}:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[right];
-    [lefttop][leftdown]vstack=inputs=2[left];
-    [left][right]hstack=inputs=2
+    [0:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[lefttop];\
+    [1:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[leftdown];\
+    [2:v]scale=-1:#{height}:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[right];\
+    [lefttop][leftdown]vstack=inputs=2[left];\
+    [left][right]hstack=inputs=2\
     """
-    |> String.replace("\n", "", global: true)
   end
 
   def default_ffmpeg_filter(width, height, 4) do
     """
-    [0:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[lefttop];
-    [1:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[righttop];
-    [2:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[leftdown];
-    [3:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[rightdown];
-    [lefttop][righttop]hstack=inputs=2[top];
-    [leftdown][rightdown]hstack=inputs=2[down];
-    [top][down]vstack=inputs=2
+    [0:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[lefttop];\
+    [1:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[righttop];\
+    [2:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[leftdown];\
+    [3:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{width}/2:ih,setsar=1[rightdown];\
+    [lefttop][righttop]hstack=inputs=2[top];\
+    [leftdown][rightdown]hstack=inputs=2[down];\
+    [top][down]vstack=inputs=2\
     """
-    |> String.replace("\n", "", global: true)
   end
 
   def default_ffmpeg_filter(width, height, 5) do
@@ -48,16 +56,15 @@ defmodule Membrane.RTC.Engine.Endpoint.HLS.FFmpegFilter do
     column_width = div(width, 3)
 
     """
-    [0:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[lefttop];
-    [1:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[middletop];
-    [2:v]scale=-1:#{height}:force_original_aspect_ratio=decrease,crop=#{column_width}+#{rest}:ih,setsar=1[right];
-    [3:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[leftdown];
-    [4:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[middledown];
-    [lefttop][middletop]hstack=inputs=2[top];[leftdown][middledown]hstack=inputs=2[bottom];
-    [top][bottom]vstack=inputs=2[left];
-    [left][right]hstack=inputs=2
+    [0:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[lefttop];\
+    [1:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[middletop];\
+    [2:v]scale=-1:#{height}:force_original_aspect_ratio=decrease,crop=#{column_width}+#{rest}:ih,setsar=1[right];\
+    [3:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[leftdown];\
+    [4:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[middledown];\
+    [lefttop][middletop]hstack=inputs=2[top];[leftdown][middledown]hstack=inputs=2[bottom];\
+    [top][bottom]vstack=inputs=2[left];\
+    [left][right]hstack=inputs=2\
     """
-    |> String.replace("\n", "", global: true)
   end
 
   def default_ffmpeg_filter(width, height, 6) do
@@ -65,17 +72,16 @@ defmodule Membrane.RTC.Engine.Endpoint.HLS.FFmpegFilter do
     column_width = div(width, 3)
 
     """
-    [0:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[lefttop];
-    [1:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[middletop];
-    [2:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}+#{rest}:ih,setsar=1[righttop];
-    [3:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[leftdown];
-    [4:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[middledown];
-    [5:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}+#{rest}:ih,setsar=1[rightdown];
-    [lefttop][middletop][righttop]hstack=inputs=3[top];
-    [leftdown][middledown][rightdown]hstack=inputs=3[bottom];
-    [top][bottom]vstack=inputs=2
+    [0:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[lefttop];\
+    [1:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[middletop];\
+    [2:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}+#{rest}:ih,setsar=1[righttop];\
+    [3:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[leftdown];\
+    [4:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}:ih,setsar=1[middledown];\
+    [5:v]scale=-1:#{height}/2:force_original_aspect_ratio=decrease,crop=#{column_width}+#{rest}:ih,setsar=1[rightdown];\
+    [lefttop][middletop][righttop]hstack=inputs=3[top];\
+    [leftdown][middledown][rightdown]hstack=inputs=3[bottom];\
+    [top][bottom]vstack=inputs=2\
     """
-    |> String.replace("\n", "", global: true)
   end
 
   def default_ffmpeg_filter(_width, _height, inputs_num) do
