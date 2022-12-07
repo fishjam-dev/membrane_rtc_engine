16271997c6cc3ed4118e8a838703c32eb2776f09
Michał Śledź <michalsledz34@gmail.com>
2022-12-07 12:56:08 +0100

    [RTC-47][RTC-48] Fix handling KeyframeRequestEvent and TrackVariantPaused events in Tee (#206)

--- test/membrane_rtc_engine/tee_test.exs
+++ test/membrane_rtc_engine/tee_test.exs
@@ -29,13 +29,7 @@ defmodule Membrane.RTC.Engine.TeeTest do
     track = build_h264_track()
     pipeline = build_video_pipeline(track, [])
 
-    Enum.each(track.variants, fn variant ->
-      actions = [event: {:output, %TrackVariantResumed{variant: variant}}]
-
-      Pipeline.execute_actions(pipeline,
-        forward: {{:source, variant}, {:execute_actions, actions}}
-      )
-    end)
+    Enum.each(track.variants, &mark_variant_as_resumed(pipeline, &1))
 
     Enum.each(track.variants, fn variant ->
       assert_sink_event(pipeline, :sink, %TrackVariantResumed{variant: ^variant})
@@ -73,15 +67,13 @@ defmodule Membrane.RTC.Engine.TeeTest do
     track = build_h264_track()
     pipeline = build_video_pipeline(track, [])
 
-    actions = [event: {:output, %TrackVariantPaused{variant: :high}}]
-    Pipeline.execute_actions(pipeline, forward: {{:source, :high}, {:execute_actions, actions}})
+    mark_variant_as_paused(pipeline, :high)
 
     assert_sink_event(pipeline, :sink, %TrackVariantPaused{variant: :high})
     refute_sink_event(pipeline, :sink, %TrackVariantPaused{variant: :medium})
     refute_sink_event(pipeline, :sink, %TrackVariantPaused{variant: :low}, 0)
 
-    actions = [event: {:output, %TrackVariantPaused{variant: :low}}]
-    Pipeline.execute_actions(pipeline, forward: {{:source, :low}, {:execute_actions, actions}})
+    mark_variant_as_paused(pipeline, :low)
 
     assert_sink_event(pipeline, :sink, %TrackVariantPaused{variant: :low})
     refute_sink_event(pipeline, :sink, %TrackVariantPaused{variant: :high})
@@ -94,13 +86,7 @@ defmodule Membrane.RTC.Engine.TeeTest do
     track = build_h264_track()
     pipeline = build_video_pipeline(track, [])
 
-    Enum.each(track.variants, fn variant ->
-      actions = [event: {:output, %TrackVariantResumed{variant: variant}}]
-
-      Pipeline.execute_actions(pipeline,
-        forward: {{:source, variant}, {:execute_actions, actions}}
-      )
-    end)
+    Enum.each(track.variants, &mark_variant_as_resumed(pipeline, &1))
 
     # wait until TrackVariantResumed is received in tee
     # (we have to assert sink in fact)
@@ -108,11 +94,10 @@ defmodule Membrane.RTC.Engine.TeeTest do
       assert_sink_event(pipeline, :sink, %TrackVariantResumed{variant: ^variant})
     end)
 
-    actions = [event: {:input, %RequestTrackVariant{variant: :high}}]
-    Pipeline.execute_actions(pipeline, forward: {:sink, {:execute_actions, actions}})
+    request_track_variant(pipeline, :high)
 
     assert_sink_event(pipeline, {:source, :high}, %Membrane.KeyframeRequestEvent{})
-    # assert there is only on KeyframeRequestEvent for h
+    # assert there is only one KeyframeRequestEvent for h
     refute_sink_event(pipeline, {:source, :high}, %Membrane.KeyframeRequestEvent{})
     refute_sink_event(pipeline, {:source, :medium}, %Membrane.KeyframeRequestEvent{}, 0)
     refute_sink_event(pipeline, {:source, :low}, %Membrane.KeyframeRequestEvent{}, 0)
@@ -132,51 +117,74 @@ defmodule Membrane.RTC.Engine.TeeTest do
     assert_receive {:DOWN, _ref, :process, ^pipeline, {:shutdown, :child_crash}}
   end
 
-  test "Tee doesn't send data until receiving RequestTrackVariant event and a keyframe for requested track variant" do
+  test "Tee correcly forwards variants" do
+    # this test checks the following things:
+    # * Tee doesn't send any variant until it is requested
+    # * when variant is requested, Tee waits for a keyframe
+    # for this variant
+    # * when variant is marked as inactive and active again,
+    # Tee doesn't send any variant until it's re-requested
+
+    request_and_check_high = fn pipeline ->
+      request_track_variant(pipeline, :high)
+      assert_sink_event(pipeline, {:source, :high}, %Membrane.KeyframeRequestEvent{})
+
+      # check whether other variants are not forwarded to the sink
+      refute_sink_buffer(pipeline, :sink, _buffer)
+
+      # send three buffers, only the last two should be received in the sink
+      [
+        %Buffer{payload: <<1, 2, 3, 4, 5>>, metadata: %{is_keyframe: false}},
+        %Buffer{payload: <<>>, metadata: %{is_keyframe: true}},
+        %Buffer{payload: <<6, 7, 8, 9, 10>>, metadata: %{is_keyframe: false}}
+      ]
+      |> Enum.each(&send_buffer(pipeline, :high, &1))
+
+      # TODO assert we receive TrackVariantSwitched before any buffer
+
+      # keyframe that we forced source to send
+      assert_sink_buffer(pipeline, :sink, %Buffer{payload: <<>>, metadata: %{is_keyframe: true}})
+      # the next buffer after the keyframe we forced source to send
+      assert_sink_buffer(pipeline, :sink, %Buffer{
+        payload: <<6, 7, 8, 9, 10>>,
+        metadata: %{is_keyframe: false}
+      })
+    end
+
     track = build_h264_track()
     pipeline = build_video_pipeline(track, {nil, &Utils.generator/2}, 3, false)
 
-    Enum.each(track.variants, fn variant ->
-      actions = [event: {:output, %TrackVariantResumed{variant: variant}}]
-
-      Pipeline.execute_actions(pipeline,
-        forward: {{:source, variant}, {:execute_actions, actions}}
-      )
-    end)
+    Enum.each(track.variants, &mark_variant_as_resumed(pipeline, &1))
 
     Enum.each(track.variants, fn variant ->
       assert_sink_event(pipeline, :sink, %TrackVariantResumed{variant: ^variant})
     end)
 
-    Enum.each(track.variants, fn variant ->
-      Pipeline.execute_actions(pipeline, forward: {{:source, variant}, {:set_active, true}})
-    end)
+    # we don't want to use generator for `:high`
+    # to have full control over the number of buffers
+    # being sent to the sink;
+    # this breaks the concept of demands but it's only
+    # for testing purposes
+    activate_source(pipeline, :low)
+    activate_source(pipeline, :medium)
 
     # tee shouldn't send us any packets
     # until we request specific track variant
     refute_sink_buffer(pipeline, :sink, _buffer)
 
-    actions = [event: {:input, %RequestTrackVariant{variant: :high}}]
-    Pipeline.execute_actions(pipeline, forward: {:sink, {:execute_actions, actions}})
-    assert_sink_event(pipeline, {:source, :high}, %Membrane.KeyframeRequestEvent{})
+    request_and_check_high.(pipeline)
+
+    mark_variant_as_paused(pipeline, :high)
+    assert_sink_event(pipeline, :sink, %TrackVariantPaused{variant: :high})
+
+    mark_variant_as_resumed(pipeline, :high)
+    assert_sink_event(pipeline, :sink, %TrackVariantResumed{variant: :high})
+
+    activate_source(pipeline, :high)
 
     refute_sink_buffer(pipeline, :sink, _buffer)
 
-    actions = [
-      buffer: {:output, %Buffer{payload: <<>>, metadata: %{is_keyframe: true}}}
-    ]
-
-    Pipeline.execute_actions(pipeline, forward: {{:source, :high}, {:execute_actions, actions}})
-
-    # TODO assert we receive TrackVariantSwitched before any buffer
-
-    # keyframe that we forced source to send
-    assert_sink_buffer(pipeline, :sink, %Buffer{payload: <<>>, metadata: %{is_keyframe: true}})
-    # some other buffer generated by source generator
-    assert_sink_buffer(pipeline, :sink, %Buffer{
-      payload: <<1, 2, 3, 4, 5>>,
-      metadata: %{is_keyframe: false}
-    })
+    request_and_check_high.(pipeline)
 
     Pipeline.terminate(pipeline, blocking?: true)
   end
@@ -185,20 +193,52 @@ defmodule Membrane.RTC.Engine.TeeTest do
     track = build_h264_track()
     pipeline = build_video_pipeline(track, [])
 
-    actions = [event: {:output, %TrackVariantPaused{variant: :high}}]
-    Pipeline.execute_actions(pipeline, forward: {{:source, :high}, {:execute_actions, actions}})
-
+    mark_variant_as_paused(pipeline, :high)
     assert_sink_event(pipeline, :sink, %TrackVariantPaused{variant: :high})
 
     Process.monitor(pipeline)
 
-    actions = [
-      buffer: {:output, %Buffer{payload: <<1, 2, 3, 4, 5>>, metadata: %{is_keyframe: false}}}
-    ]
-
-    Pipeline.execute_actions(pipeline, forward: {{:source, :high}, {:execute_actions, actions}})
+    buffer = %Buffer{payload: <<1, 2, 3, 4, 5>>, metadata: %{is_keyframe: false}}
+    send_buffer(pipeline, :high, buffer)
 
     assert_receive {:DOWN, _ref, :process, ^pipeline, {:shutdown, :child_crash}}
+  end
+
+  test "Tee ignores KeyframeRequestEvent when there is no variant being forwarded" do
+    track = build_h264_track()
+    pipeline = build_video_pipeline(track, [])
+
+    request_keyframe(pipeline)
+
+    refute_sink_event(pipeline, {:source, :high}, %Membrane.KeyframeRequestEvent{})
+    refute_sink_event(pipeline, {:source, :medium}, %Membrane.KeyframeRequestEvent{}, 0)
+    refute_sink_event(pipeline, {:source, :low}, %Membrane.KeyframeRequestEvent{}, 0)
+
+    Pipeline.terminate(pipeline, blocking?: true)
+  end
+
+  test "Tee forwards KeyframeRequestEvent when there is some variant being forwarded" do
+    track = build_h264_track()
+    pipeline = build_video_pipeline(track, [])
+
+    Enum.each(track.variants, &mark_variant_as_resumed(pipeline, &1))
+
+    Enum.each(track.variants, fn variant ->
+      assert_sink_event(pipeline, :sink, %TrackVariantResumed{variant: ^variant})
+    end)
+
+    request_track_variant(pipeline, :high)
+
+    buffer = %Buffer{payload: <<>>, metadata: %{is_keyframe: true}}
+    send_buffer(pipeline, :high, buffer)
+
+    request_keyframe(pipeline)
+
+    assert_sink_event(pipeline, {:source, :high}, %Membrane.KeyframeRequestEvent{})
+    refute_sink_event(pipeline, {:source, :medium}, %Membrane.KeyframeRequestEvent{})
+    refute_sink_event(pipeline, {:source, :low}, %Membrane.KeyframeRequestEvent{}, 0)
+
+    Pipeline.terminate(pipeline, blocking?: true)
   end
 
   defp build_h264_track() do
@@ -239,5 +279,34 @@ defmodule Membrane.RTC.Engine.TeeTest do
 
     Pipeline.execute_actions(pipeline, actions)
     pipeline
+  end
+
+  defp mark_variant_as_resumed(pipeline, variant) do
+    actions = [event: {:output, %TrackVariantResumed{variant: variant}}]
+    Pipeline.execute_actions(pipeline, forward: {{:source, variant}, {:execute_actions, actions}})
+  end
+
+  defp mark_variant_as_paused(pipeline, variant) do
+    actions = [event: {:output, %TrackVariantPaused{variant: variant}}]
+    Pipeline.execute_actions(pipeline, forward: {{:source, variant}, {:execute_actions, actions}})
+  end
+
+  defp request_track_variant(pipeline, variant) do
+    actions = [event: {:input, %RequestTrackVariant{variant: variant}}]
+    Pipeline.execute_actions(pipeline, forward: {:sink, {:execute_actions, actions}})
+  end
+
+  defp send_buffer(pipeline, variant, buffer) do
+    actions = [buffer: {:output, buffer}]
+    Pipeline.execute_actions(pipeline, forward: {{:source, variant}, {:execute_actions, actions}})
+  end
+
+  defp activate_source(pipeline, variant) do
+    Pipeline.execute_actions(pipeline, forward: {{:source, variant}, {:set_active, true}})
+  end
+
+  defp request_keyframe(pipeline) do
+    actions = [event: {:input, %Membrane.KeyframeRequestEvent{}}]
+    Pipeline.execute_actions(pipeline, forward: {:sink, {:execute_actions, actions}})
   end
 end
