45fe6413d9913e6f11b3c7e1c8488a3a81208a05
Michał Śledź <michalsledz34@gmail.com>
2022-12-09 16:20:48 +0100

    Bump version to 0.8.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTC.Engine.MixProject do
   use Mix.Project
 
-  @version "0.8.1"
+  @version "0.8.2"
   @github_url "https://github.com/membraneframework/membrane_rtc_engine"
 
   def project do
