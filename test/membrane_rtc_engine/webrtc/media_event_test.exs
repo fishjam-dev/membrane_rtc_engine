184fb6b7d71225c193b5ea1655ab5d00d329f85b
Łukasz Wala <lukasz.wala@swmansion.com>
2023-01-25 14:23:21 +0100

    Add media event tests

--- test/membrane_rtc_engine/webrtc/media_event_test.exs
+++ test/membrane_rtc_engine/webrtc/media_event_test.exs
@@ -39,4 +39,121 @@ defmodule Membrane.RTC.Engine.Endpoint.WebRTC.MediaEventTest do
       assert {:error, :invalid_media_event} == MediaEvent.decode(raw_media_event)
     end
   end
+
+  describe "deserializing sdpOffer media event" do
+    setup do
+      %{
+        sdp: "mock_sdp",
+        track_id: "mock_track_id",
+        track_metadata: %{"abc" => "cba"},
+        max_bandwidth: %{"l" => 100, "m" => 200, "h" => 300},
+        mid: 5
+      }
+    end
+
+    test "creates proper map when event is valid", context do
+      raw_media_event =
+        %{
+          "type" => "custom",
+          "data" => %{
+            "type" => "sdpOffer",
+            "data" => %{
+              "sdpOffer" => %{
+                "type" => "offer",
+                "sdp" => context.sdp
+              },
+              "trackIdToTrackInfo" => %{
+                context.track_id => %{
+                  "trackMetadata" => context.track_metadata,
+                  "maxBandwidth" => context.max_bandwidth,
+                  "mid" => context.mid
+                }
+              }
+            }
+          }
+        }
+        |> Jason.encode!()
+
+      expected_media_event = %{
+        type: :custom,
+        data: %{
+          type: :sdp_offer,
+          data: %{
+            sdp_offer: %{
+              type: :offer,
+              sdp: context.sdp
+            },
+            track_id_to_track_info: %{
+              context.track_id => %{
+                track_metadata: context.track_metadata,
+                max_bandwidth: context.max_bandwidth,
+                mid: context.mid
+              }
+            }
+          }
+        }
+      }
+
+      assert {:ok, expected_media_event} == MediaEvent.decode(raw_media_event)
+    end
+
+    test "returns error when event misses key", context do
+      raw_media_event =
+        %{
+          "type" => "custom",
+          "data" => %{
+            "type" => "sdpOffer",
+            "data" => %{
+              "sdpOffer" => %{
+                "type" => "offer",
+                "sdp" => context.sdp
+              },
+              "trackIdToTrackInfo" => %{
+                context.track_id => %{
+                  "trackMetadata" => context.track_metadata,
+                  # missing maxBandwidth key
+                  "mid" => context.mid
+                }
+              }
+            }
+          }
+        }
+        |> Jason.encode!()
+
+      assert {:error, :invalid_media_event} == MediaEvent.decode(raw_media_event)
+    end
+  end
+
+  describe "deserializing trackVariantBitrates media event" do
+    test "creates proper map when event is valid" do
+      track_id = "mock_track_id"
+      bitrates = %{"h" => 1000, "m" => 500}
+
+      raw_media_event =
+        %{
+          "type" => "custom",
+          "data" => %{
+            "type" => "trackVariantBitrates",
+            "data" => %{
+              "trackId" => track_id,
+              "variantBitrates" => bitrates
+            }
+          }
+        }
+        |> Jason.encode!()
+
+      expected_media_event = %{
+        type: :custom,
+        data: %{
+          type: :track_variant_bitrates,
+          data: %{
+            track_id: track_id,
+            variant_bitrates: bitrates
+          }
+        }
+      }
+
+      assert {:ok, expected_media_event} == MediaEvent.decode(raw_media_event)
+    end
+  end
 end
