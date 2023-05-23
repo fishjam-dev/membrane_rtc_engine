667791b9effea9a94e93034a0679ea80b213c50b
blazpie <blazej.pierzak@gmail.com>
2023-05-23 14:34:07 +0200

    resolve conflicts

--- mix.exs
+++ mix.exs
@@ -61,8 +61,8 @@ defmodule Membrane.RTC.Engine.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.11.3"},
-      {:membrane_webrtc_plugin, "~> 0.14.4"},
+      {:membrane_core, "~> 0.11.2"},
+      {:membrane_webrtc_plugin, "~> 0.14.5"},
       {:membrane_rtp_plugin, "~> 0.22.0"},
       {:membrane_rtp_format, "~> 0.6.0"},
       {:membrane_tee_plugin, "~> 0.10.0"},
