e903154d8439db1e55bdd88aa43e98c3d808acdd
Radosław Szuma <56085570+Rados13@users.noreply.github.com>
2023-10-18 10:19:32 +0200

    Update engine/lib/membrane_rtc_engine/track.ex
    Co-authored-by: Michał Śledź <michalsledz34@gmail.com>

--- engine/lib/membrane_rtc_engine/track.ex
+++ engine/lib/membrane_rtc_engine/track.ex
@@ -60,7 +60,7 @@ defmodule Membrane.RTC.Engine.Track do
   * `active?` - indicates whether track is still available or not (because client left a room)
   * `metadata` - any data passed by user to be linked with this track
   * `ctx` - any data Endpoints need to associate with `#{inspect(__MODULE__)}.t()` for internal usage
-  * `framerate` - framerate of video track used only when framerate is constant otherwise it should be set to nil. By default nil
+  * `framerate` - track framerate.  It's up to endpoints whether they set this field or not but if they do, they should only set it for video tracks with constant framerate. For audio tracks, framerate should always be `nil`. Defaults to `nil`.
   """
   @type t :: %__MODULE__{
           type: :audio | :video,
