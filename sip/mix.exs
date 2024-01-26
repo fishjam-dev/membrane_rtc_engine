687dd9d375bb5112844541521b8d72df8fb6b460
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2024-01-26 16:12:29 +0100

    Update lockfiles

--- sip/mix.exs
+++ sip/mix.exs
@@ -51,8 +51,8 @@ defmodule Membrane.RTC.Engine.Endpoint.SIP.MixProject do
   defp deps do
     [
       # Engine deps
-      {:membrane_rtc_engine, path: "../engine"},
-      {:membrane_rtc_engine_webrtc, path: "../webrtc"},
+      {:membrane_rtc_engine, "~> 0.20.0"},
+      {:membrane_rtc_engine_webrtc, "~> 0.6.0"},
 
       # Regular deps
       {:membrane_core, "~> 1.0"},
