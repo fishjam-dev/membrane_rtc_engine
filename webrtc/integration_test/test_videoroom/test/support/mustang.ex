113820d8695b51e24db2b4560ec3e15a1e1fdfed
Radosław Szuma <radoslawszuma@gmail.com>
2024-02-13 11:39:40 +0100

    Update package.json

--- webrtc/integration_test/test_videoroom/test/support/mustang.ex
+++ webrtc/integration_test/test_videoroom/test/support/mustang.ex
@@ -63,7 +63,7 @@ defmodule TestMustang do
 
   defp get_stats(page, receiver, browser_id, tag, button) do
     :ok = Playwright.Page.click(page, "[id=#{button}]")
-    Process.sleep(500)
+    Process.sleep(1_000)
 
     page
     |> Playwright.Page.text_content("[id=data]")
