1f06c4c21ddfbf73193fb1901142feb564e6d8b1
Radosław Szuma <radoslawszuma@gmail.com>
2024-03-26 09:23:06 +0100

    Fix dialyzer issue

--- recording/lib/recording_endpoint.ex
+++ recording/lib/recording_endpoint.ex
@@ -185,7 +185,7 @@ defmodule Membrane.RTC.Engine.Endpoint.Recording do
 
   defp save_reports(reporter, stores, recording_id) do
     case Reporter.get_report(reporter) do
-      %{tracks: %{}, recording_id: _recoridng_id} ->
+      %{tracks: tracks, recording_id: _recoridng_id} when tracks == %{} ->
         Membrane.Logger.warning("No tracks in report, doesn't save it in storage")
 
       report ->
