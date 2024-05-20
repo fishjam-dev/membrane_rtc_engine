2614c07f1827421bc53248042e75f94a1cedd455
Karol Konkol <konkolisz@gmail.com>
2024-05-20 11:38:45 +0200

    Remove dependency overrides

--- engine/mix.exs
+++ engine/mix.exs
@@ -55,9 +55,7 @@ defmodule Membrane.RTC.Engine.MixProject do
     [
       {:membrane_core, "~> 1.0"},
       {:membrane_rtp_plugin,
-       github: "membraneframework/membrane_rtp_plugin",
-       branch: "rtcp-sender-packet",
-       override: true},
+       github: "membraneframework/membrane_rtp_plugin", branch: "rtcp-sender-packet"},
       {:membrane_rtp_format, "~> 0.8.0"},
       {:ex_sdp, "~> 0.15.0"},
       {:elixir_uuid, "~> 1.2"},
